bae804ea625a026c4413982f8404ecd15c78d8a6
hometlt <visceroi@gmail.com>
2022-09-28 12:43:43 +0400

    quick-fix

--- server.mjs
+++ server.mjs
@@ -2,23 +2,20 @@ import  {YandexDisk} from 'yandex-disk'
 let disk = new YandexDisk('y0_AgAAAAABtKHXAAhrwwAAAADPIH6Z4KcCiFKYQx-3gqSOJYYNt54ipbQ');
 
 import express from  'express'
+import serveStatic from 'serve-static'
 import cors from 'cors'
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
-import yargs from 'yargs'
 const __dirname = dirname(fileURLToPath(import.meta.url));
 process.chdir(__dirname)
 
 
-let port = process.argv[3] || 3000;
-
-
-
-
+let port = process.env.PORT || process.argv[3] || 5000
 
 
 let app = express()
 app.use(cors());
+app.use(serveStatic(__dirname))
 app.use((req, res, next) => { console.log('Time: %d', Date.now()); next()})
 
 disk.responseFile = function(srcPath, response, callback) {
@@ -37,3 +34,5 @@ app.get('/download/:file', function(req, res){
     })
 });
 app.listen(port)
+
+console.log('server started ' + port)
